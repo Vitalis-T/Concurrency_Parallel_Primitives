d97e5879b6243458ceb5879ea230b6b3b6db6d0e
Vitalis <vitalis.tkachenk0@gmail.com>
2021-02-20 11:57:47 +0200

    bloop log

--- src/main/scala/org/learningconcurrency/ExecutionContextSleep.scala
+++ src/main/scala/org/learningconcurrency/ExecutionContextSleep.scala
@@ -2,9 +2,9 @@ package org.learningconcurrency
 import scala.concurrent._
 
 object ExecutionContextSleep extends App {
-	for (i <- 0 until 32) execute {
-		Thread.sleep(2000)
-		log(s"Task $i completed")
-	}
-	Thread.sleep(34000)
+  for (i <- 0 until 32) execute {
+    Thread.sleep(2000)
+    log(s"Task $i completed")
+  }
+  Thread.sleep(34000)
 }
